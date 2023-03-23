25c1e395543bdd2c24ba3b8c39f8473682c2a40e
Greg Cain <codegregcode@gmail.com>
2023-03-23 14:37:36 +0000

    corrects test and modifies component to pass

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -2,13 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ForecastSummary(props) {
-  const { date, temperature, description, icon } = props;
+  const { date, description, temperature, icon } = props;
 
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__description">{description}</div>
-      <div className="forecast-summary__icon">{icon}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
       <div className="forecast-summary__temperature">
         {temperature.max}
         &deg;C
